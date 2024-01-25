a1485fef90fe13eb58cafb97793f27a572d96d13
91639 <anuragofficial.wrk@gmail.com>
2024-01-25 14:40:01 +0530

    used connectionimpl class

--- JDBC_CRUD/src/com/JPMorgan/Main/MainClass.java
+++ JDBC_CRUD/src/com/JPMorgan/Main/MainClass.java
@@ -1,11 +1,17 @@
 package com.JPMorgan.Main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		System.out.println("HI Anurag Kaushik");
-
+		Connection conn = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cuims_my","","");
+		System.out.println("Connection Established");
+		System.out.println(conn.getClass().getName());
 	}
-
 }
